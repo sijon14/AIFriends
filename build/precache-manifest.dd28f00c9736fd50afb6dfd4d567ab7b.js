23d28a7930800c1b2cde4d6ac69d3df84805c139
Sijon <sijon14@hotmail.co.uk>
2019-03-13 11:57:01 +0000

    Add files via upload

---
+++ build/precache-manifest.dd28f00c9736fd50afb6dfd4d567ab7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b8d0adf67de2b6c562b9",
+    "url": "/static/js/main.5afd7826.chunk.js"
+  },
+  {
+    "revision": "ea5525698348946ee012",
+    "url": "/static/js/2.7cb68033.chunk.js"
+  },
+  {
+    "revision": "b8d0adf67de2b6c562b9",
+    "url": "/static/css/main.6fb76373.chunk.css"
+  },
+  {
+    "revision": "ea5525698348946ee012",
+    "url": "/static/css/2.b58bc597.chunk.css"
+  },
+  {
+    "revision": "ded711e313db2aa51fc13e510f80eb53",
+    "url": "/index.html"
+  }
+];
